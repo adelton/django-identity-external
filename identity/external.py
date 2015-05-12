f7e70c3160fb568ceae4b360151855269cb6f755
Jan Pazdziora <jan.pazdziora@django.adelton.com>
2015-05-12 20:03:17 +0200

    Add support for colon-separated REMOTE_USER_GROUPS.

--- identity/external.py
+++ identity/external.py
@@ -17,11 +17,14 @@ class RemoteUserAttrMiddleware(RemoteUserMiddleware):
 
 		ext_groups = []
 		ext_group_count = request.META.get(self.header + "_GROUP_N", None)
+		ext_groups_singlestring = request.META.get(self.header + "_GROUPS", None)
 		if ext_group_count is not None:
 			for i in range(1, int(ext_group_count) + 1):
 				g = request.META.get(self.header + "_GROUP_" + str(i), None)
 				if g is not None:
 					ext_groups.append(g)
+		elif ext_groups_singlestring is not None:
+			ext_groups = ext_groups_singlestring.split(':')
 		else:
 			return
 
@@ -63,6 +66,6 @@ class RemoteUserAttrMiddleware(RemoteUserMiddleware):
 				if lastname is not None:
 					user.last_name = lastname
 					need_save = True
-				if need_save or request.META.get(self.header + "_GROUP_N", None):
+				if need_save or request.META.get(self.header + "_GROUP_N", None) or request.META.get(self.header + "_GROUPS", None):
 					self.update_user_groups(request)
 					user.save()

2e72976741ebeed7bd97fa53a66d6b6fb049d965
Tibor Dudlák <tibor.dudlak@gmail.com>
2017-03-29 15:13:32 +0200

    Metadata for setuptools.

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README')) as readme:
+	README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+	name = 'django-identity-external',
+	version = '0.5',
+	packages = find_packages(),
+	include_package_data = True,
+	license = 'Apache 2.0 License',
+	description = 'Django middleware for handling of external identities.',
+	long_description = README,
+	url = 'https://github.com/adelton/django-identity-external',
+	author = 'Jan Pazdziora',
+	author_email = 'jan.pazdziora@django.adelton.com',
+	classifiers = [
+		'Environment :: Web Environment',
+		'Framework :: Django',
+		# New PersistentRemoteUserMiddleware in Django 1.9
+		'Framework :: Django :: 1.9',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: Apache Software License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2.7'
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Topic :: Internet :: WWW/HTTP',
+		'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+	],
+)

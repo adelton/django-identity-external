20ec9840e29f587323f5c689fb9eb4e67c0cf42e
Jan Pazdziora <jan.pazdziora@django.adelton.com>
2023-03-15 23:22:11 +0100

    Add a default openid scope to give access to the UserInfo endpoint.
    Addressing
    setup_1        | Checking OpenID Connect UserInfo endpoint.
    keycloak_1     | 2023-03-15 21:36:36,141 WARN  [org.keycloak.events] (executor-thread-0) type=USER_INFO_REQUEST_ERROR, realmId=3c5a014b-0e0f-4c6e-bfd5-9fd9309300ce, clientId=null, userId=null, ipAddress=192.168.80.5, error=access_denied, auth_method=validate_access_token
    setup_1        | curl: (22) The requested URL returned error: 403
    setup_1        | 403
    django-identity-external_setup_1 exited with code 22
    
    Related to https://github.com/keycloak/keycloak/pull/14237.

--- tests/setup.sh
+++ tests/setup.sh
@@ -40,6 +40,10 @@ echo Creating two users.
 "${CURL[@]}" -d '{"username":"bob","firstName":"Robert","lastName":"Chase","email":"robert.chase@example.test","enabled":true,"credentials":[{"type":"password","value":"bobovo heslo","temporary":false}],"groups":["group-3","group-1"]}' $BASE/admin/realms/realm-b/users
 "${CURL[@]}" -d '{"username":"david","firstName":"David","lastName":"Křížala","email":"davidk@example.test","enabled":true,"credentials":[{"type":"password","value":"davidovo heslo","temporary":false}],"groups":["group-2","admins"]}' $BASE/admin/realms/realm-b/users
 
+echo Creating openid scope, setting it as default.
+"${CURL[@]}" -d '{"name":"openid","id":"d499dfe7-fea4-4d40-ba1b-e0b895798ef2","protocol":"openid-connect","attributes":{"include.in.token.scope": "true"}}' $BASE/admin/realms/realm-b/client-scopes
+"${CURL[@]}" -X PUT $BASE/admin/realms/realm-b/default-default-client-scopes/d499dfe7-fea4-4d40-ba1b-e0b895798ef2
+
 echo Creating one client.
 "${CURL[@]}" -d '{"id":"http-app-x","redirectUris":["http://www:8080/openidc-redirect-uri"],"frontchannelLogout":true,"attributes":{"post.logout.redirect.uris":"http://www:8080/admin/logout/?keycloak-logged-out"},"clientAuthenticatorType":"client-secret","secret":"client-secret-very-secret","directAccessGrantsEnabled":true,"protocolMappers":[{"name":"groups","protocol":"openid-connect","protocolMapper":"oidc-group-membership-mapper","config":{"full.path": "false","claim.name":"groups","userinfo.token.claim":"true"}}]}' $BASE/admin/realms/realm-b/clients
 
@@ -48,6 +52,9 @@ BOB_TOKEN=$( curl -s --fail-with-body -d "grant_type=password&client_id=http-app
 BOB_CURL+=(curl -H "Authorization: Bearer $BOB_TOKEN" -H "Content-Type: application/json" --no-progress-meter --fail-with-body -w '%{http_code}\n')
 "${BOB_CURL[@]}" $BASE/realms/realm-b/protocol/openid-connect/userinfo | jq
 # "${CURL[@]}" $BASE/admin/realms/realm-b/clients/http-app-x | jq
+DAV_TOKEN=$( curl -s --fail-with-body -d "grant_type=password&client_id=http-app-x&client_secret=client-secret-very-secret&username=david&password=davidovo+heslo" $BASE/realms/realm-b/protocol/openid-connect/token | jq -r '.access_token' )
+DAV_CURL+=(curl -H "Authorization: Bearer $DAV_TOKEN" -H "Content-Type: application/json" --no-progress-meter --fail-with-body -w '%{http_code}\n')
+"${DAV_CURL[@]}" $BASE/realms/realm-b/protocol/openid-connect/userinfo | jq
 
 curl --no-progress-meter $WWW | grep -q Django
 

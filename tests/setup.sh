02283f829f5fc5f2e9974eac262ad8fc3e646d6c
Jan Pazdziora <jpazdziora@redhat.com>
2023-03-29 21:28:41 +0200

    Use the setup.sh parameters consistently.

--- tests/setup.sh
+++ tests/setup.sh
@@ -45,7 +45,7 @@ echo Creating openid scope, setting it as default.
 "${CURL[@]}" -X PUT $BASE/admin/realms/realm-b/default-default-client-scopes/d499dfe7-fea4-4d40-ba1b-e0b895798ef2
 
 echo Creating one client.
-"${CURL[@]}" -d '{"id":"http-app-x","redirectUris":["http://www:8080/openidc-redirect-uri"],"frontchannelLogout":true,"attributes":{"post.logout.redirect.uris":"http://www:8080/admin/logout/?keycloak-logged-out"},"clientAuthenticatorType":"client-secret","secret":"client-secret-very-secret","directAccessGrantsEnabled":true,"protocolMappers":[{"name":"groups","protocol":"openid-connect","protocolMapper":"oidc-group-membership-mapper","config":{"full.path": "false","claim.name":"groups","userinfo.token.claim":"true"}}]}' $BASE/admin/realms/realm-b/clients
+"${CURL[@]}" -d '{"id":"http-app-x","redirectUris":["'$WWW'/openidc-redirect-uri"],"frontchannelLogout":true,"attributes":{"post.logout.redirect.uris":"'$WWW'/admin/logout/?keycloak-logged-out"},"clientAuthenticatorType":"client-secret","secret":"client-secret-very-secret","directAccessGrantsEnabled":true,"protocolMappers":[{"name":"groups","protocol":"openid-connect","protocolMapper":"oidc-group-membership-mapper","config":{"full.path": "false","claim.name":"groups","userinfo.token.claim":"true"}}]}' $BASE/admin/realms/realm-b/clients
 
 echo Checking OpenID Connect UserInfo endpoint.
 BOB_TOKEN=$( curl -s --fail-with-body -d "grant_type=password&client_id=http-app-x&client_secret=client-secret-very-secret&username=bob&password=bobovo+heslo" $BASE/realms/realm-b/protocol/openid-connect/token | jq -r '.access_token' )
